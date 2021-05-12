5ca735dbad5a71dfc11c80ccb11e58e14f51802c
anac29 <anaconmar@alum.us.es>
2021-05-12 10:54:19 +0200

    Comparators
    He añadido los 4 comparators nuevos para tarea

---
+++ src/main/java/aiss/api/resources/comparators/ComparatortituloTarea.java
@@ -0,0 +1,14 @@
+package aiss.api.resources.comparators;
+
+import java.util.Comparator;
+
+import aiss.model.Tarea;
+
+public class ComparatortituloTarea implements Comparator<Tarea>{
+	
+	public int compare(Tarea t1, Tarea t2) {
+		// TODO Auto-generated method stub
+		return t1.getTitulo().compareTo(t2.getCategoria());
+	}
+
+}
