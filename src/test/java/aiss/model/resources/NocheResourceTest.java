ffe3a031eeb74ba6c571dcda7b13939168114261
Jaime Moscoso Bernal <80198997+jaimemosber@users.noreply.github.com>
2021-05-25 11:54:34 +0200

    Update NocheResourceTest.java

--- src/test/java/aiss/model/resources/NocheResourceTest.java
+++ src/test/java/aiss/model/resources/NocheResourceTest.java
@@ -8,103 +8,118 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.restlet.resource.ResourceException;
 
-import aiss.model.Song;
+import aiss.model.CalidadSuenyo;
+import aiss.model.EstadoAnimo;
+import aiss.model.Noche;
+import aiss.model.TipoSuenyo;
+import aiss.api.resources.NochesResource;
 
 public class NocheResourceTest {
 
-	static Song song1, song2, song3;
-	static SongResource sr = new SongResource();
+	static Noche noche1, noche2, noche3;
+	static NochesResource sr = new NochesResource();
 	
 	@BeforeClass
 	public static void setup() throws Exception {
 		
-		// Test song 1
-		song1 = sr.addSong(new Song("Test title","Test artist","Test album","2016"));
+		// Test noche 1
+		noche1 = sr.addNoche(new Noche("n12","03/11/2020",TipoSuenyo.PROFUNDO, CalidadSuenyo.BUENA, EstadoAnimo.FELIZ,22,6));
 		
-		// Test song 2
-		song2 = sr.addSong(new Song("Test title 2","Test artist 2","Test album 2","2017"));
+		// Test noche 2
+		noche2 = sr.addNoche(new Noche("n13","04/11/2020",TipoSuenyo.PROFUNDO, CalidadSuenyo.BUENA, EstadoAnimo.FELIZ,00,8));
 		
 	}
 
 	@AfterClass
 	public static void tearDown() throws Exception {
-		sr.deleteSong(song1.getId());
-		sr.deleteSong(song3.getId());
+		sr.deleteNoche(noche1.getId());
+		sr.deleteNoche(noche3.getId());
 	}
 	
 	@Test
 	public void testGetAll() {
-		Collection<Song> songs = sr.getAll();
+		Collection<Noche> noches = sr.getAll();
 		
-		assertNotNull("The collection of songs is null", songs);
+		assertNotNull("La colección de noches es nula.", noches);
 		
 		// Show result
-		System.out.println("Listing all songs:");
+		System.out.println("Lista de todas las noches:");
 		int i=1;
-		for (Song s : songs) {
-			System.out.println("Song " + i++ + " : " + s.getTitle() + " (ID=" + s.getId() + ")");
+		for (Noche n : noches) {
+			System.out.println("Noche " + i++ + " : " + n.getFecha_suenyo() + " (ID=" + n.getId() + ")");
 		}
 	}
 
 	@Test
-	public void testGetSong() {
-		Song s = sr.getSong(song1.getId());
+	public void testGetNoche() {
+		Noche n = sr.getNoche(noche1.getId());
 		
-		assertEquals("The id of the songs do not match", song1.getId(), s.getId());
-		assertEquals("The name of the songs do not match", song1.getTitle(), s.getTitle());
+		assertEquals("Los id de las noches no coinciden", noche1.getId(), n.getId());
+		assertEquals("La fecha de las noches no coinciden", noche1.getCalidadsuenyo(), n.getCalidadsuenyo());
 		
 		// Show result
-		System.out.println("Song id: " +  s.getId());
-		System.out.println("Song name: " +  s.getTitle());	
+		System.out.println("Noche id: " +  n.getId());
+		System.out.println("Noche fecha: " +  n.getFecha_suenyo());	
 		}
 
 	@Test
-	public void testAddSong() {
+	public void testAddNoche() {
 		
-		Song song4 = new Song("Entre dos aguas","Paco de Lucia","Entre dos aguas","2021");
-		Song added = sr.addSong(song4);
+		Noche noche4 = new Noche("n15","03/11/2020",TipoSuenyo.PROFUNDO, CalidadSuenyo.BUENA, EstadoAnimo.FELIZ,22,6);
+
+		Noche added = sr.addNoche(noche4);
 		
-		assertNotNull("Error when adding the song", song4);
-		assertEquals("The song's name has not been setted correctly", added.getTitle(), song4.getTitle());
-		assertEquals("The song's album has not been setted correctly", added.getAlbum(), song4.getAlbum());
-		assertEquals("The song's year has not been setted correctly", added.getYear(), song4.getYear());
+		assertNotNull("Error añadiendo la noche", noche4);
+		assertEquals("La fecha de la noche no se ha asignado correctamente.", added.getFecha_suenyo(), noche4.getFecha_suenyo());
+		assertEquals("La calidad del sueño de la noche no se ha asignado correctamente.", added.getCalidadsuenyo(), noche4.getCalidadsuenyo());
+		assertEquals("El estado de ánimo no se ha asignado correctamente.", added.getEstado_animo(), noche4.getEstado_animo());
+		assertEquals("La hora de inicio no se ha asignado correctamente.", added.getHora_in(), noche4.getHora_in());
+		assertEquals("La hora de fin no se ha asignado correctamente.", added.getHora_fin(), noche4.getHora_fin());
+		assertEquals("El tipo de sueño no se ha asignado correctamente.", added.getTipo_suenyo(), noche4.getTipo_suenyo());
 	}
 
 	@Test
-	public void testUpdateSong() {
+	public void testUpdateNoche() {
 		
-		String songTitle = "Update song test title";
-		String songArtist = "Update song test artist";
-		String songAlbum = "Update song test album";
-		String songYear = "1995";
+		String fechaSuenyo = "13/12/2020";
+		TipoSuenyo tipoSuenyo = TipoSuenyo.MEDIO;
+		CalidadSuenyo calidadSuenyo = CalidadSuenyo.MALA;
+		EstadoAnimo estadoAnimo = EstadoAnimo.CANSADO;
+		Integer hora_in = 3;
+		Integer hora_fin = 4;
 		
-		// Update song
-		song1.setTitle(songTitle);
-		song1.setArtist(songArtist);
-		song1.setAlbum(songAlbum);
-		song1.setYear(songYear);
+		// Update noche
+		noche1.setFecha_suenyo("09/10/2019");
+		noche1.setTipo_suenyo(TipoSuenyo.MEDIO);
+		noche1.setCalidadsuenyo(CalidadSuenyo.MUYMALA);
+		noche1.setEstado_animo(EstadoAnimo.ENFADADO);
+		noche1.setHora_in(2);
+		noche1.setHora_fin(8);
 		
-		boolean success = sr.updateSong(song1);
+		boolean success = sr.updateNoche(noche1);
 		
-		assertTrue("Error when updating the song", success);
+		assertTrue("Error actualizando la noche", success);
 		
-		Song song  = sr.getSong(song1.getId());
-		assertEquals("The song's title has not been updated correctly", songTitle, song.getTitle());
-		assertEquals("The song's artist has not been updated correctly", songArtist, song.getArtist());
-		assertEquals("The song's album has not been updated correctly", songAlbum, song.getAlbum());
-		assertEquals("The song's year has not been updated correctly", songYear, song.getYear());
+		Noche noche = sr.getNoche(noche1.getId());
+		
+		assertEquals("La fecha de la noche no se ha actualizado correctamente.", fechaSuenyo, noche.getFecha_suenyo());
+		assertEquals("La calidad del sueño de la noche no se ha actualizado correctamente.", calidadSuenyo, noche.getCalidadsuenyo());
+		assertEquals("El estado de ánimo no se ha actualizado correctamente.", estadoAnimo, noche.getEstado_animo());
+		assertEquals("La hora de inicio no se ha actualizado correctamente.", hora_in, noche.getHora_in());
+		assertEquals("La hora de fin no se ha actualizado correctamente.", hora_fin, noche.getHora_fin());
+		assertEquals("El tipo de sueño no se ha actualizado correctamente.", tipoSuenyo, noche.getTipo_suenyo());
 	}
 
 	@Test(expected = ResourceException.class)
-	public void testDeleteSong() {
+	public void testDeleteNoche() {
 		
 		// Delete songs
-		boolean success = sr.deleteSong(song2.getId());
+		boolean success = sr.deleteNoche(noche2.getId());
 		
-		assertTrue("Error when deleting the song", success);
+		assertTrue("Error eliminando la noche", success);
 		
-		Song song  = sr.getSong(song2.getId());
-		assertNull("The song has not been deleted correctly", song);
+		Noche noche = sr.getNoche(noche2.getId());
+		assertNull("La noche no se ha añadido correctamente", noche);
 	}
 
 }
