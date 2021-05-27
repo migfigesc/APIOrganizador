5e5ffa90ac5937c6811abbc3b9cd8da17dda8037
Enrique Muñoz <73706577+enrmp@users.noreply.github.com>
2021-05-27 12:21:55 +0200

    test

---
+++ java/aiss/model/resourcestest/SongResourceTest.java
@@ -0,0 +1,111 @@
+package aiss.model.resourcestest;
+
+import static org.junit.Assert.*;
+import java.util.Collection;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.restlet.resource.ResourceException;
+
+import aiss.model.Song;
+import aiss.model.resources.SongResource;
+
+public class SongResourceTest {
+
+	static Song song1, song2, song3;
+	static SongResource sr = new SongResource();
+	
+	@BeforeClass
+	public static void setup() throws Exception {
+		
+		// Test song 1
+		song1 = sr.addSong(new Song("Test title","Test artist","Test album","2016"));
+		
+		// Test song 2
+		song2 = sr.addSong(new Song("Test title 2","Test artist 2","Test album 2","2017"));
+		
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		sr.deleteSong(song1.getId());
+		sr.deleteSong(song3.getId());
+	}
+	
+	@Test
+	public void testGetAll() {
+		Collection<Song> songs = sr.getAll();
+		
+		assertNotNull("The collection of songs is null", songs);
+		
+		// Show result
+		System.out.println("Listing all songs:");
+		int i=1;
+		for (Song s : songs) {
+			System.out.println("Song " + i++ + " : " + s.getTitle() + " (ID=" + s.getId() + ")");
+		}
+	}
+
+	@Test
+	public void testGetSong() {
+		Song s = sr.getSong(song1.getId());
+		
+		assertEquals("The id of the songs do not match", song1.getId(), s.getId());
+		assertEquals("The name of the songs do not match", song1.getTitle(), s.getTitle());
+		
+		// Show result
+		System.out.println("Song id: " +  s.getId());
+		System.out.println("Song name: " +  s.getTitle());	
+		}
+
+	@Test
+	public void testAddSong() {
+		
+		Song song4 = new Song("Entre dos aguas","Paco de Lucia","Entre dos aguas","2021");
+		Song added = sr.addSong(song4);
+		
+		assertNotNull("Error when adding the song", song4);
+		assertEquals("The song's name has not been setted correctly", added.getTitle(), song4.getTitle());
+		assertEquals("The song's album has not been setted correctly", added.getAlbum(), song4.getAlbum());
+		assertEquals("The song's year has not been setted correctly", added.getYear(), song4.getYear());
+	}
+
+	@Test
+	public void testUpdateSong() {
+		
+		String songTitle = "Update song test title";
+		String songArtist = "Update song test artist";
+		String songAlbum = "Update song test album";
+		String songYear = "1995";
+		
+		// Update song
+		song1.setTitle(songTitle);
+		song1.setArtist(songArtist);
+		song1.setAlbum(songAlbum);
+		song1.setYear(songYear);
+		
+		boolean success = sr.updateSong(song1);
+		
+		assertTrue("Error when updating the song", success);
+		
+		Song song  = sr.getSong(song1.getId());
+		assertEquals("The song's title has not been updated correctly", songTitle, song.getTitle());
+		assertEquals("The song's artist has not been updated correctly", songArtist, song.getArtist());
+		assertEquals("The song's album has not been updated correctly", songAlbum, song.getAlbum());
+		assertEquals("The song's year has not been updated correctly", songYear, song.getYear());
+	}
+
+	@Test(expected = ResourceException.class)
+	public void testDeleteSong() {
+		
+		// Delete songs
+		boolean success = sr.deleteSong(song2.getId());
+		
+		assertTrue("Error when deleting the song", success);
+		
+		Song song  = sr.getSong(song2.getId());
+		assertNull("The song has not been deleted correctly", song);
+	}
+
+}
